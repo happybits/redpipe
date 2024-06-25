be7d654aa599c86b95eb94b62dd97425d52a1b3c
Andy Kretschmar <akretschmar@marcopolo.me>
2024-06-25 15:40:06 -0500

    fix a mypy error; update mypy & flake8; drop python 3.7 and 3.8 support

--- setup.sh
+++ setup.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-command -v virtualenv >/dev/null 2>&1 || { echo >&2 "I require virtualenv but it's not installed.  Aborting."; exit 1; }
-
 root_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 venv_dir="$root_dir/.venv"
 
-
 if [ ! -f "$venv_dir/bin/python" ]
 then
     echo "configuring virtualenv $venv_dir ..."
-    virtualenv -q "$venv_dir" || { echo >&2 "unable to configure the virtualenv for the project in $venv_dir"; exit 1; }
+    python -m venv "$venv_dir" || { echo >&2 "unable to configure the virtualenv for the project in $venv_dir"; exit 1; }
 fi
 
 "$venv_dir/bin/python" -m pip install --upgrade pip
@@ -20,7 +17,7 @@ then
     exit 1
 fi
 
-"$venv_dir/bin/pip" install -q -r "$root_dir/dev-requirements.txt"
+"$venv_dir/bin/pip" install -q -r "$root_dir/local-dev-requirements.txt"
 
 if [ $? -ne 0 ]
 then
@@ -28,3 +25,4 @@ then
     exit 1
 fi
 
+source "$venv_dir/bin/activate"
