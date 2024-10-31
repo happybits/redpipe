4e3def4b4d8dcd0fccc0a0e422ade1ceffbbf713
Andy Kretschmar <akretschmar@marcopolo.me>
2024-10-31 16:49:54 -0500

    add comments & constants around cluster scanning

--- redpipe/redis_cluster.py
+++ redpipe/redis_cluster.py
@@ -7,6 +7,15 @@ from typing_extensions import TypeAlias
 
 KeyType: TypeAlias = Union[bytes, str, memoryview]
 
+# for cluster scans, we use the lower 48 bits of the cursor to represent the
+# actual cursor value returned by a node.  The zero-based index of the node
+# (based on sorting the node by name in the cluster) is stored in the bits
+# starting at 48.  This allows us to transparently scan across shard
+# boundaries even though each individual node's Redis instance will follow
+# the usual cursor=0 -> cursor=0 cycle.
+NODE_IDX_OFFSET_BITS = 48
+CURSOR_MASK = 0xffffffffffff
+
 
 class CustomClusterPipeline(redis.cluster.ClusterPipeline):
     def __init__(self, *args, **kwargs):
@@ -18,8 +27,8 @@ class CustomClusterPipeline(redis.cluster.ClusterPipeline):
              count: Optional[int] = None,
              _type: Optional[str] = None,  # for lint/type-checking purposes
              **kwargs) -> Union[Awaitable, Any]:
-        shard_idx = cursor >> 48
-        cursor = cursor & 0xffffffffffff
+        shard_idx = cursor >> NODE_IDX_OFFSET_BITS
+        cursor = cursor & CURSOR_MASK
 
         # TODO: would be nice to be able to specify reading from replicas
         node = self.get_sorted_primaries()[shard_idx]
@@ -39,7 +48,7 @@ class CustomClusterPipeline(redis.cluster.ClusterPipeline):
         if new_cursor == 0:
             shard_idx += 1
         if shard_idx < len(nodes):
-            new_cursor = (shard_idx << 48) | new_cursor
+            new_cursor = (shard_idx << NODE_IDX_OFFSET_BITS) | new_cursor
 
         return new_cursor, keys
 
